73f07eeae65ce9a0f3f220056ecd1e425f319d25
jdreyes <jreyes@autentia.com>
2015-07-13 09:28:49 +0200

    Rest of test cases in stringSplitter

--- src/test/java/com/autentia/cursos/tdd/StringSplitterTest.java
+++ src/test/java/com/autentia/cursos/tdd/StringSplitterTest.java
@@ -5,16 +5,55 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.fail;
 
 public class StringSplitterTest {
 
     private StringSplitter stringSplitterSUT = new StringSplitter();
 
-    //Prueba con null
+    @Test(expected = NullPointerException.class)
+    public void shouldNotSplitStringsWithNullArgument() {
 
-    //Prueba base ""
+        //given
+        String nullString = null;
 
-    //Prueba base "a"
+        //when
+        final String[] stringParts = stringSplitterSUT.split(nullString);
+
+        //then
+        fail("Not expected. String split arguments should throw exception if it's null");
+
+    }
+
+
+    @Test
+    public void shouldSplitStringsWithEmptyArgumentAndReturnArrayWithEmptyString() {
+
+        //given
+        String emptyString = "";
+
+        //when
+        final String[] stringParts = stringSplitterSUT.split(emptyString);
+
+        //then
+        assertThat(stringParts.length, is(1));
+        assertThat(stringParts[0], is(""));
+    }
+
+    @Test
+    public void shouldSplitStringsWithSingleArgument() {
+
+        //given
+        String singlePartString = "a";
+
+        //when
+        final String[] stringParts = stringSplitterSUT.split(singlePartString);
+
+        //then
+        assertThat(stringParts.length, is(1));
+        assertThat(stringParts[0], is("a"));
+    }
+
 
     @Test
     public void shouldSplitStringsWhenAStringWithCommasIsGiven() {
